07dcfe8e8beac29fa552209b14d0796e68ba92eb
linneasellerholm <linneasm@vgy.se>
2016-09-16 14:03:29 +0200

    hej

--- for-satsen/Program.cs
+++ for-satsen/Program.cs
@@ -10,6 +10,25 @@ namespace for_satsen
     {
         static void Main(string[] args)
         {
+            //Övning 1
+            for (int x = 0; x < 101; x++)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadKey();
+
+            //Övning 2
+            for (int x = 20; x > 0; x--)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadKey();
+            //Övning 3
+            for (int i = 0; i < 50; i=i+2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
         }
     }
 }
